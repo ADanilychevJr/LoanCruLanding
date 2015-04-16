68765a969ce4a31912b6149268396f0df32b13dd
ADanilychevJr <danilychev@berkeley.edu>
2015-04-15 18:33:40 -0700

    Trying out creating own php email subscription

---
+++ signup.php
@@ -0,0 +1,37 @@
+<?php
+
+
+
+$username = $_POST['user'];
+
+$email = $_POST['mail'];
+
+$experience = $_POST['exp'];
+
+
+
+//the data
+
+$data = "$username | $email | $experience\n";
+
+
+
+//open the file and choose the mode
+
+$fh = fopen("users.txt", "a");
+
+fwrite($fh, $data);
+
+
+
+//close the file
+
+fclose($fh);
+
+
+
+print "User Submitted";
+
+
+
+?>
